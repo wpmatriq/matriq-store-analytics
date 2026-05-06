6e1a861fb24b9a4005b035abb001eeff956ecb93
Navanath Bhosale <navanathb@bsf.io>
2026-05-06 09:19:58 +0530

    build

--- assets/build/wc-sma-app.asset.php
+++ assets/build/wc-sma-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => 'c575e348afd9c4b2bec7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => 'afaca8c48b62ca3795ec');
